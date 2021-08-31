8efac1432903aad9d34713c74a1ec01886a12a4e
amalnasr <amalalghanim96@gmail.com>
2021-08-31 23:02:30 +0300

    update database path

--- test_app.py
+++ test_app.py
@@ -12,11 +12,11 @@ class CapstoneTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "capstoneproject"
-        self.database_path = "postgresql://{}@{}/{}".format('postgres:1996', 'localhost:5432', self.database_name)
-        # database_path = os.environ['DATABASE_URL']
-        # if database_path.startswith("postgres://"):
-        #  database_path = database_path.replace("postgres://", "postgresql://", 1)
+        # self.database_name = "capstoneproject"
+        # self.database_path = "postgresql://{}@{}/{}".format('postgres:1996', 'localhost:5432', self.database_name)
+        database_path = os.environ['DATABASE_URL']
+        if database_path.startswith("postgres://"):
+         database_path = database_path.replace("postgres://", "postgresql://", 1)
         setup_db(self.app, self.database_path)
         
         self.casting_assistant_header = {
